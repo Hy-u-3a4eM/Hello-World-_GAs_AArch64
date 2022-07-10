a0b4232dadc19be4ac8649667d528406faea1057
Hy-u-3a4eM <za4emsu@gmail.com>
2022-07-10 14:30:14 +0300

    Bugfix

--- Hello-World-/Hello-World-.s
+++ Hello-World-/Hello-World-.s
@@ -3,21 +3,26 @@
 	.p2align	2
 	.type	_start,@function
 _start:
-	mov	x0, xzr
-	ldr	x1, =Hello_World_
+	mov	x0, #1
+	//ldr	x1, =Hello_World_
+	adrp	x1, Hello_World_
+	add	x1, x1, :lo12:Hello_World_
 	mov	x2, #Hello_World_len
-	mov	w8, #64
+	mov	x8, #64
 	svc	#0
 
 	mov	x0, xzr
-	mov	w8, #93
+	mov	x8, #93
 	svc	#0
+
+	mov	w0, wzr
+	ret
 .Lfunc_end0:
 	.size	_start, .Lfunc_end0-_start
 
 
 	.type	Hello_World_,@object
-	.data
+	.section	.rodata,"a",@progbits
 	.globl	Hello_World_
 Hello_World_:
 	.asciz	"Hello, World!\n"
